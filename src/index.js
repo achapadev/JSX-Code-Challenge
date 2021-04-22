ac5313907467a34b437a0be28d4e073e7363c9af
achapadev <83032999+achapadev@users.noreply.github.com>
2021-04-22 14:46:38 -0500

    created react app from scratch. Has a h1 heading and 3 list items.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Adrian's favorite games</h1>
+    <ul>
+      <li>WoW</li>
+      <li>LoL</li>
+      <li>Call of Duty</li>
+    </ul>
+  </div>,
+  document.querySelector("#root")
+);
